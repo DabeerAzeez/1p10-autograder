324010b3a921b50c15ff23c8ac8e9f6afc026a05
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-12-23 16:44:43 -0500

    Update documentation

--- TestCases/testcaseUpdater.py
+++ TestCases/testcaseUpdater.py
@@ -1,18 +1,37 @@
 """
-Welcome to testcaseUpdater
-author: Dabeer Abdul-Azeez (abdulazd@mcmaster.ca)
+1P10 Autograder Test Case Updater
+Author: Dabeer Abdul-Azeez <abdulazd@mcmaster.ca>
 
-This script automatically determines the appropriate outputs for testcases which are listed in an excel file (the
-"test case workbook" or TCWB) and then populates the file appropriately. The completed TCWB can then be used for
-comparing student submissions to the correct answers.
+This script automatically determines the appropriate outputs for test cases which are listed in the
+"test case workbook" (hereafter TCWB) and then populates the TCWB appropriately. The completed TCWB can then be used by
+the autograder script as an answer key.
+
+Requirements to run this script:
+- TCWB containing individual sheets for each computing lab which requires test cases
+    - Check TCWB for instructions on how to write test cases
+    - "Input cells" refer to all cells which require input in order for this script to run (i.e. function name,
+       test case weight, inputs)
+- Solution Python scripts (and any helper scripts for those) which house the functions referenced in the TCWB
+
+This script will:
+- Ask for a specific sheet of the TCWB (i.e. a specific milestone) whose test case outputs need updating
+- Run through the appropriate sheet of the TCWB, passing the listed inputs through the listed functions and placing
+the outputs in the appropriate column
+- Sort all sheets in the TCWB alphabetically (to prevent sheets from going out of order when updating)
 
 Notes:
 - This version uses xlrd v1.2.0; newer versions do not support xlsx files
-- Don't have any excel files open otherwise permission errors will occur!
-- The inputs in the TCWB are assumed to be using python syntax (e.g. 4**2 instead of 4^2)
+- Don't have any excel files open otherwise permission errors will occur when attempting to write to them
+- Instructions on how to write test cases are included in the TCWB
 """
 
-# TODO: Don't use old xlrd (https://stackoverflow.com/questions/65250207/pandas-cannot-open-an-excel-xlsx-file)
+# TODO: Don't rely on old xlrd (https://stackoverflow.com/questions/65250207/pandas-cannot-open-an-excel-xlsx-file)
+# TODO: Documentation:
+'''
+https://stackoverflow.com/questions/3898572/what-is-the-standard-python-docstring-format
+https://numpydoc.readthedocs.io/en/latest/
+https://www.python.org/dev/peps/pep-0257/
+'''
 
 import pandas as pd
 import importlib
@@ -27,7 +46,15 @@ sheet_names_df = pd.DataFrame(test_case_xl.sheet_names, columns=['Sheet Name'])
 
 def select_sheet():
     """
-    :return: Name of selected sheet within test case workbook
+    Allows user to select a sheet from the TCWB for updating
+
+    Parameters
+    ----------
+    sheet_names_df: Dataframe containing names of sheets in TCWB
+
+    Returns
+    -------
+    Name of selected sheet within TCWB
     """
     print(sheet_names_df, "\n")
     chosen_index = int(input("Please select the row number of the sheet you'd like to update: "))
@@ -37,11 +64,17 @@ def select_sheet():
 
 def perform_test(parameters, function, solution_module):
     """
-    :param parameters: Parameters from 'Inputs' column of test case worksheet; type int or string, string could be one
-                       input or multiple delimited values
-    :param function: Function to run the test on
-    :param solution_module: Solution file module from which to retrieve function
-    :return: output(s) of function
+    Retrieves the output of a function from the solution script when the test case parameters are passed
+
+    Parameters
+    ----------
+    parameters: Inputs from test case worksheet; type int/float/string (one input or multiple delimited values)
+    function: Function to test
+    solution_module: Solution module from which to retrieve function
+
+    Returns
+    -------
+    Output(s) of function
     """
     for item in [parameters, function, solution_module]:
         if pd.isna(item):
@@ -80,6 +113,14 @@ def perform_tests(chosen_sheet):
     the appropriate functions from the appropriates Python solution file.
     :param chosen_sheet: Chosen sheet of test cases workbook
     """
+    Auto-fills 'Outputs' column of selected sheet in test cases workbook by passing the inputs through
+    the appropriate functions from the appropriates Python solution file.
+
+    Parameters
+    ----------
+    test_case_xl: Pandas ExcelFile object of TCWB
+    chosen_sheet: Chosen sheet of test cases workbook
+    """
     test_cases_df = pd.read_excel(test_case_xl, sheet_name=chosen_sheet)  # Read chosen sheet
     solution_module = importlib.import_module(chosen_sheet + "_SOLUTION")  # Import appropriate solution module
 
@@ -95,7 +136,7 @@ def perform_tests(chosen_sheet):
     book.remove(book[chosen_sheet])  # Remove original sheet
     writer.book = book  # Update writer's book
 
-    test_cases_df.to_excel(writer, chosen_sheet, index=False)
+    test_cases_df.to_excel(writer, chosen_sheet, index=False)  # Write updated test case sheet to excel file
 
     try:
         writer.save()
